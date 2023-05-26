cf5593b27935dcea779b3390a706368f2f8d3801
Jack Crowley <jcrowley.jjc@gmail.com>
2023-05-26 08:33:09 -0400

    visualizer basic GUI

---
+++ visualizer.js
@@ -0,0 +1,22 @@
+let running = false;
+const sb = document.querySelector(".start");
+const eb = document.querySelector(".end");
+
+sb.addEventListener("click", startSimulation)
+eb.addEventListener("click", endSimulation)
+
+function startSimulation() {
+    running = true;
+    
+    sb.style.display = "none";
+    eb.style.display = "block";
+
+    
+}
+
+function endSimulation() {
+    running = false;
+
+    sb.style.display = "block";
+    eb.style.display = "none";
+}
