6b9cc8438a0ba07b2e4268e6e9dc9a6040ba42f4
Jack Crowley <jcrowley.jjc@gmail.com>
2023-05-26 08:51:03 -0400

    start genetic algorithm + added comments

--- player.js
+++ player.js
@@ -8,6 +8,7 @@ class Player {
 
         this.nn = null; // Create neural net with first layer headers in inputsHeaders and weights
         // Prob 3 layer (1 hidden) but not too sure
+        // Weights and inputHeaders both arrays, with the 0 index of both representating same obj
     }
 
     // Calculates inputs and stores in this.inputs
@@ -32,5 +33,14 @@ class Player {
     get fitness() {
         // Placeholder
         return Math.random()*200-100;
+    }
+
+    createClones(number) {
+        let clonesWeights = [];
+        for (let i = 0; i < number; i++) {
+            clonesWeights.push();
+        }
+
+        return clonesWeights;
     }
 }
