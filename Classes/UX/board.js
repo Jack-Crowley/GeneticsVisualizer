12acc68fcbcd7d5d9bb1dc599d847cf95a33a543
ParJai <parthjain22@gmail.com>
2023-06-12 10:08:23 -0400

    chart tracking

--- Classes/UX/board.js
+++ Classes/UX/board.js
@@ -23,7 +23,8 @@ class Board {
 
     sortPositions(player, name) {
         let list = [];
-        this.getShapes(name).forEach((shape) => {            shape.positionToPlayer = Math.sqrt((shape.position.xPos-player.shape.position.xPos)**2+(shape.position.yPos-player.shape.position.yPos)**2);
+        this.getShapes(name).forEach((shape) => {
+            shape.positionToPlayer = Math.sqrt((shape.position.xPos-player.shape.position.xPos)**2+(shape.position.yPos-player.shape.position.yPos)**2);
             list.push(shape);
         })
         list.sort(Shape.compareTo);
@@ -41,6 +42,13 @@ class Board {
         this.shapes.forEach((shape) => {
             shape.update()
         })
+        // let bestMover = game.getBestAgent()
+        // document.querySelectorAll('canvas.chart').forEach((canvas) => {
+        //     canvas.dataset.values = canvas.dataset.values.split(',').slice(0,-1).join(',')
+        //     canvas.dataset.values += bestMover.score
+        //     console.log("Added: " + canvas.dataset.values)
+        //     createChart()
+        // })
     }
 
     deleteShape(shape) {
--- simulation.js
+++ simulation.js
@@ -8,19 +8,27 @@ let interval = -1;
 function start(simulation) {
     table.innerHTML=''
     simulation.textContent = 0;
+    numAgents = 10
 
     running = true;
 
     if (simulation) {
-        game = new Tester(simulation, 100);
+        game = new Tester(simulation, numAgents);
         interval = setInterval(() => {
             if (game.frameNum >= 500) {
                 simulationNum.textContent = parseInt(simulationNum.textContent)+1
                 table.innerHTML="";
                 bestMovers = game.getBestAgents(2)
                 console.log("First: "+bestMovers[0].score+"; "+"Second: "+bestMovers[1].score+"; Average: "+((bestMovers[0].score+bestMovers[1].score)/2))
+                document.querySelectorAll('canvas.chart').forEach((canvas) => {
+                    if (canvas.dataset.values !== "") canvas.dataset.values += ","
+                    // canvas.dataset.values = canvas.dataset.values.split(',').slice(0,-1).join(',')
+                    canvas.dataset.values += bestMovers[0].score
+                    createChart()
+                    console.log(canvas.dataset.values)
+                })
                 game.end();
-                game = new Tester(simulation, 100);
+                game = new Tester(simulation, numAgents);
             }
         }, 0)
     }
