5d2ec06518956351c25657588ff12bbce1245f54
Jacob Berger <tkdpenguin123@gmail.com>
2023-06-19 18:27:42 -0400

    chart is now visible and tabs are implemented between chart and stats

--- visualizer.js
+++ visualizer.js
@@ -85,3 +85,23 @@ function autoExpand(textarea) {
     textarea.style.height = 'auto'; 
     textarea.style.height = textarea.scrollHeight + 'px'; 
 }
+
+document.getElementById("defaultOpen").click();
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
