192c1b6c72eefac93f1b739a85eeaf495708520c
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-06-19 21:32:07 -0400

    player

--- Classes/Games/createGame.js
+++ Classes/Games/createGame.js
@@ -6,7 +6,10 @@ let json = {
     "timed":[]
 }
 
+let err = "err"
+
 let playerOption = ["X","Y","RADIUS","STOKE_COLOR","FILL_COLOR"]
+let playerRequired = ["X","Y","RADIUS","STOKE_COLOR"]
 
 function loadJSON() {
 
@@ -23,22 +26,40 @@ function loadJSON() {
     })
 }
 
-function getFormatedNumber() {
-    
+function getFormatedNumber(num) {
+    if (typeof num == 'number') return num;
+    if (!num.startsWith("random(")) return err;
+    let sections = num.split(",")
+    if (sections.length != 2) return err;
+    let leftNum = sections[0].split("(")[1]
+    if (typeof leftNum != 'number') return err;
+    let rightNum = sections[1].split(")")[0]
+    if (typeof rightNum != 'number') return err;
+    return `random(${leftNum},${rightNum})`
 }
 
 function playerStuff(text) {    
+    let vars = new Map();
     text.forEach((line) => {
         if (line == "") return;
         let word = line.split(" ")
-        if (word.length != 3) return;
-        if (word[0].toUpperCase() != "SET") return;
-        if (playerOption.indexOf(word[1].toUpperCase()) == -1) return;
-
+        if (word.length != 3) return err;
+        if (word[0].toUpperCase() != "SET") return err;
+        if (playerOption.indexOf(word[1].toUpperCase()) == -1) return err;
         if (playerOption.indexOf(word[1].toUpperCase()) <= 2) {
-
+            let formattedNum = getFormatedNumber(word[2]);
+            if (formattedNum == err) return err;
+            vars.put(word[1].toLowerCase(), formattedNum)
         }
     })
+
+    playerRequired.forEach((requiredOption) => {
+        if (!vars.has(requiredOption.toLowerCase())) return err;
+    })
+
+    for (const [key, value] of vars.entries()) {
+        json.player[key] = value
+    }
 }
 
 function shapeStuff(text) {
